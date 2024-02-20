1f3b0e3b519405d92ae69dbd5de74ac2b9c8e18c
Thomas Chou <thomas@wytron.com.tw>
2024-02-20 09:48:29 -0800

    board: pico_me maxrpm 200

--- config/custom/pico_me_config.h
+++ config/custom/pico_me_config.h
@@ -54,7 +54,7 @@ ROBOT ORIENTATION
 */
 
 //define your robot' specs here
-#define MOTOR_MAX_RPM 180                   // motor's max RPM
+#define MOTOR_MAX_RPM 200                   // motor's max RPM
 #define MAX_RPM_RATIO 0.85                  // max RPM allowed for each MAX_RPM_ALLOWED = MOTOR_MAX_RPM * MAX_RPM_RATIO
 #define MOTOR_OPERATING_VOLTAGE 12          // motor's operating voltage (used to calculate max RPM)
 #define MOTOR_POWER_MAX_VOLTAGE 12          // max voltage of the motor's power source (used to calculate max RPM)
